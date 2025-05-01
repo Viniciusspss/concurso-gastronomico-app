94a88db1624c8b3d0f76f8afe5ee625240516c47
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-01 16:07:18 -0300

    "style: ajusta largura do layout e centraliza div do SelectProfile"

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 export function AuthLayout() {
     return (
         <div className="flex flex-col w-full h-screen items-center justify-center bg-[#F2F2F2]">
-            <div className="w-[400px] px-4">
+            <div className="w-[500px] px-4">
                 <Outlet />
             </div>
         </div>
--- src/pages/auth/SelectProfile.tsx
+++ src/pages/auth/SelectProfile.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function SelectProfile() {
 
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center w-full">
             <h1 className="mb-4 text-2xl font-medium">Escolha como deseja realizar login</h1>
             <div className="flex flex-col gap-4 justify-center items-center">
                 <Button className="bg-amber-50 w-60 shadow-lg" asChild>
