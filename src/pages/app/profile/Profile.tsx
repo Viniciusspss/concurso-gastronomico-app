9d3400047cf7c31fde47a5b2f09ee850380fa4cd
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-03 21:19:11 -0300

    "feat: priva rota de visualizar perfil"

--- src/pages/app/profile/Profile.tsx
+++ src/pages/app/profile/Profile.tsx
@@ -1,60 +1,13 @@
-import { DefaultButton } from "@/components/DefaultButton";
-import { DefaultForm } from "@/components/DefaultForm";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { ProfileForm } from "@/components/ProfileForm";
+import { useAuthContext } from "@/context/authContext/useAuthContext";
+import { Navigate } from "react-router-dom";
 
 export function Profile() {
-  return (
-    <div className="flex w-full max-w-lg flex-col items-center justify-center gap-10 px-4">
-      <h1 className="text-2xl text-amber-50">PERFIL</h1>
-      <DefaultForm>
-        <div className="flex flex-col gap-2">
-          <Label className="text-amber-50" htmlFor="firstName">
-            Primeiro Nome:
-          </Label>
-          <Input
-            className="bg-amber-50"
-            id="firstName"
-            placeholder="Primeiro nome do usuário"
-          ></Input>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label className="text-amber-50" htmlFor="lastName">
-            Último Nome:
-          </Label>
-          <Input
-            className="bg-amber-50"
-            id="lastName"
-            placeholder="Último nome do usuário"
-          ></Input>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label className="text-amber-50" htmlFor="email">
-            Email:
-          </Label>
-          <Input
-            className="bg-amber-50"
-            id="email"
-            placeholder="Email do usuário"
-          ></Input>
-        </div>
-        <div className="flex justify-between">
-          <Link to="/EditProfile">
-            <DefaultButton className="min-w-[100px] px-4 text-xs">
-              EDITAR
-            </DefaultButton>
-          </Link>
-          <DefaultButton className="min-w-[100px] px-4 text-xs">
-            EXCLUIR
-          </DefaultButton>
-          <Link to="/Dishes">
-            <DefaultButton className="min-w-[100px] px-4 text-xs">
-              FECHAR
-            </DefaultButton>
-          </Link>
-        </div>
-      </DefaultForm>
-    </div>
-  );
+  const { user } = useAuthContext();
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
+  return <ProfileForm />;
 }
