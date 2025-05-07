f1763c12531c5ad3a9cc44487e7b5ae877c30bf0
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-07 18:50:05 -0300

    feat: cria mock para restaurantes

---
+++ src/data/restaurants.ts
@@ -0,0 +1,52 @@
+import { RestaurantType } from "@/types/user/restaurant";
+
+export const restaurants: RestaurantType[] = [
+  {
+    id: "1",
+    cnpj: "00",
+    name: "BDC",
+    password: "123",
+    dishes: [
+      {
+        imageURL: "https://",
+        title: "TÍTULO DO PRATO",
+        description:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: 35,
+        restaurant: {
+          name: "BDC",
+        },
+      },
+      {
+        imageURL: "https://",
+        title: "TÍTULO DO PRATO",
+        description:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: 35,
+        restaurant: {
+          name: "BDC",
+        },
+      },
+      {
+        imageURL: "https://",
+        title: "TÍTULO DO PRATO",
+        description:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: 35,
+        restaurant: {
+          name: "BDC",
+        },
+      },
+      {
+        imageURL: "https://",
+        title: "TÍTULO DO PRATO",
+        description:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: 35,
+        restaurant: {
+          name: "BDC",
+        },
+      },
+    ],
+  },
+];
