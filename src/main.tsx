17a53d559f4a83d541b38b6a30d8e12ab7379324
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-07 21:38:13 -0300

    feat: utiliza context de pratos na main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./context/authContext/AuthContextProvider.tsx";
+import { DishContextProvider } from "./context/dishContext/DishContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
-      <App />
+      <DishContextProvider>
+        <App />
+      </DishContextProvider>
     </AuthContextProvider>
   </StrictMode>,
 );
