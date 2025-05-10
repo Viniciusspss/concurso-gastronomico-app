2282585c6c35cf7d27b47d7f78cb9480810f15d4
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-10 15:19:48 -0300

    style: estiliza dialog de exclusão de prato

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,30 +1,32 @@
 import { ToastContentProps } from "react-toastify";
 import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
 
-import { DefaultButton } from "./DefaultButton";
+import { Button } from "./ui/button";
 
 export function Dialog({ closeToast, data }: ToastContentProps<string>) {
   return (
     <>
-      <div>
+      <div className="z-10">
         <p>{data}</p>
 
-        <div>
-          <DefaultButton
+        <div className="flex items-center justify-center gap-4">
+          <Button
             onClick={() => closeToast(true)}
             aria-label="Confirmar ação e fechar"
             title="Confirmar ação e fechar"
+            className="bg-emerald-500 hover:bg-emerald-700"
           >
             <ThumbsUpIcon />
-          </DefaultButton>
-          <DefaultButton
+          </Button>
+          <Button
+            className="bg-red-500 hover:bg-red-700"
             onClick={() => closeToast(false)}
             color="red"
             aria-label="Cancelar ação e fechar"
             title="Cancelar ação e fechar"
           >
             <ThumbsDownIcon />
-          </DefaultButton>
+          </Button>
         </div>
       </div>
     </>
