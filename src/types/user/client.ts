92fc34dd4fc4a04a0aff55a2f7d1d03d106f3494
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-18 22:07:58 -0300

    feat: adiciona schema de validação zod para cliente

--- src/types/user/client.ts
+++ src/types/user/client.ts
@@ -1,7 +1,11 @@
-export type ClientType = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-};
+import { z } from "zod";
+
+export const clientSchema = z.object({
+  id: z.string().uuid(),
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6, "Senha deve ter no mínimo 8 caracteres"),
+});
+
+export type ClientType = z.infer<typeof clientSchema>;
