9df760cefe3ad677ba836f1caa5c128737ef00c7
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-24 01:36:13 -0300

    refactor: configura axios corretamente

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,16 +1,10 @@
 import axios from "axios";
-import { store } from "@/store/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
-
-api.interceptors.request.use((config) => {
-  const token = store.getState().auth.token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
