c43b75fc4ae0b325f0148a7d4c977aaa1a24398b
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-26 20:23:35 -0300

    feat: tipa corretamente campo de imagem e cria formDta com os dados do formulario

--- src/components/CreateDishForm.tsx
+++ src/components/CreateDishForm.tsx
@@ -10,49 +10,44 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { clearError } from "@/store/slices/dishSlice/dishSlice";
-
-type createDishFormData = {
-    image_url: string,
-    price: string,
-    details: string,
-    name: string
-}
+import { createDishFormData } from "@/types/dishes";
 
 export function CreateDishForm() {
-    const { user } = useAppSelector(state => state.auth)
-    const { errorCreateDish } = useAppSelector(state => state.dishes)
-    const dispatch = useAppDispatch()
-    const [price, setPrice] = useState("")
-    const { register, handleSubmit } = useForm<createDishFormData>()
-    const navigate = useNavigate()
+    const { user } = useAppSelector((state) => state.auth);
+    const { errorCreateDish } = useAppSelector((state) => state.dishes);
+    const dispatch = useAppDispatch();
+    const [price, setPrice] = useState("");
+    const { register, handleSubmit, setValue } = useForm<createDishFormData>();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (errorCreateDish) {
-            toast.dismiss()
-            toast.error(errorCreateDish)
-            dispatch(clearError())
+            toast.dismiss();
+            toast.error(errorCreateDish);
+            dispatch(clearError());
         }
-    }, [errorCreateDish, user?.id, dispatch])
+    }, [errorCreateDish, user?.id, dispatch]);
 
-
-
-
-    const handleCreateDish = ({ details, image_url, name, price }: createDishFormData) => {
+    const handleCreateDish = ({
+        details,
+        image_url,
+        name,
+        price,
+    }: createDishFormData) => {
         if (!user?.id) return;
-        dispatch(
-            createDish({
-                name,
-                price: parseFloat(price).toFixed(2),
-                details,
-                image_url,
-            }),
-        ).then((action) => {
+        const toStringPrice = parseFloat(price).toFixed(2);
+        const data = new FormData();
+        data.append("name", name);
+        data.append("price", toStringPrice);
+        data.append("image", image_url);
+        data.append("details", details);
+        dispatch(createDish(data)).then((action) => {
             if (createDish.fulfilled.match(action)) {
-                toast.success("Prato cadastrado com sucesso!")
+                toast.success("Prato cadastrado com sucesso!");
                 navigate(`/restaurant-dishes/${user?.id}`);
             }
-        })
-    }
+        });
+    };
 
     return (
         <div className="flex w-full max-w-lg flex-col items-center justify-center gap-10 px-4">
@@ -81,14 +76,18 @@ export function CreateDishForm() {
                         min={0}
                         step={0.01}
                         {...register("price")}
-                        onChange={e => setPrice(e.target.value)}
+                        onChange={(e) => setPrice(e.target.value)}
                     ></Input>
                 </div>
                 <div className="flex flex-col gap-2">
                     <Label className="text-amber-50" htmlFor="details">
                         Descrição:
                     </Label>
-                    <textarea className="bg-amber-50 text-black" id="details"  {...register("details")} />
+                    <textarea
+                        className="bg-amber-50 text-black"
+                        id="details"
+                        {...register("details")}
+                    />
                 </div>
                 <div className="flex flex-col gap-2">
                     <Label className="text-amber-50" htmlFor="image_url">
@@ -97,8 +96,13 @@ export function CreateDishForm() {
                     <Input
                         className="bg-amber-50"
                         id="image_url"
-                        placeholder="Insira o link da imagem do prato"
-                        {...register("image_url")}
+                        type="file"
+                        onChange={(e) => {
+                            const file = e.target.files?.[0];
+                            if (file) {
+                                setValue("image_url", file, { shouldValidate: true });
+                            }
+                        }}
                     ></Input>
                 </div>
 
@@ -111,8 +115,7 @@ export function CreateDishForm() {
                         FECHAR
                     </DefaultButton>
                 </Link>
-
-            </DefaultForm >
+            </DefaultForm>
         </div>
-    )
+    );
 }
