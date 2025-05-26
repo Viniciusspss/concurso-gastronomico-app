cff2c3d2e4bbe2eb7f985d641174990774d8773d
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-25 23:14:43 -0300

    feat: cria logica do refreshToken e acessToken e deleta contentType json

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,9 +2,50 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const response = await axios.post(
+          "http://localhost:8080/user/refresh-token",
+          {
+            refreshToken,
+          },
+        );
+
+        const newAccessToken = response.data.accessToken;
+        localStorage.setItem("accessToken", newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest); // tenta de novo
+      } catch (err) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/";
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
