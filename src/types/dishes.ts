5da51a9e531a336481586134a944c9d80f167e2b
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-26 19:53:25 -0300

    feat: cria schema para validar busca de todos os pratos

--- src/types/dishes.ts
+++ src/types/dishes.ts
@@ -2,10 +2,12 @@ import { z } from "zod";
 
 export const dishesSchema = z.object({
   id: z.string().uuid(),
-  imageURL: z.string().url(),
-  title: z.string().min(1),
-  description: z.string().min(5),
-  price: z.preprocess((val) => Number(val), z.number().positive()),
+  image_url: z.string().url(),
+  name: z.string().min(1),
+  details: z.string().min(5),
+  price: z.string().refine((val) => /^\d+(\.\d{2})$/.test(val), {
+    message: "Preço deve estar no formato 0.00",
+  }),
 });
 
 export const dishesWithRestaurantSchema = dishesSchema.extend({
@@ -14,5 +16,46 @@ export const dishesWithRestaurantSchema = dishesSchema.extend({
   }),
 });
 
+export const getAllDishesResponseSchema = dishesSchema.extend({
+  restaurant: z.object({
+    select: z.object({
+      id: z.string().uuid(),
+      name: z.string().min(1),
+      image_url: z.string().min(1),
+    }),
+  }),
+  reviews: z.object({
+    select: z.object({
+      id: z.string().uuid(),
+      rating: z
+        .number({
+          required_error: "A nota é obrigatória.",
+          invalid_type_error: "A nota deve ser um número.",
+        })
+        .min(1, {
+          message: "A nota mínima é 1.",
+        })
+        .max(5, {
+          message: "A nota máxima é 5.",
+        }),
+      comment: z
+        .string({
+          required_error: "O comentário é obrigatório.",
+        })
+        .min(1, {
+          message: "O comentário não pode estar vazio.",
+        }),
+      user: z.object({
+        select: z.object({
+          id: z.string().uuid(),
+          first_name: z.string(),
+          last_name: z.string(),
+        }),
+      }),
+    }),
+  }),
+});
+
 export type DishesType = z.infer<typeof dishesSchema>;
 export type DishesWithRestaurant = z.infer<typeof dishesWithRestaurantSchema>;
+export type getAllDishesResponse = z.infer<typeof getAllDishesResponseSchema>;
