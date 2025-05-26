734ee986ff7b7421259038c3a05c76ba251c1967
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-25 23:21:03 -0300

    feat: adiciona campo de imagem no type do restaurant

--- src/types/user/restaurant.ts
+++ src/types/user/restaurant.ts
@@ -1,16 +1,28 @@
 import { z } from "zod";
-import { dishesSchema } from "../dishes";
+import { dishesWithRestaurantSchema } from "../dishes";
 
 export const restaurantSchema = z.object({
   id: z.string().uuid(),
   cnpj: z.string(),
   name: z.string(),
   password: z.string(),
-  dishes: z.array(dishesSchema),
+  dishes: z.array(dishesWithRestaurantSchema),
+  image_url: z
+    .instanceof(File, { message: "Imagem é obrigatória" })
+    .refine((file) => file.size > 0, {
+      message: "O arquivo não pode estar vazio",
+    }),
 });
 
 export const restaurantFormSchema = restaurantSchema.omit({
   id: true,
   dishes: true,
 });
+
+export const signUpFormDataSchema = restaurantSchema
+  .omit({ id: true, dishes: true })
+  .extend({
+    repeatPassword: z.string(),
+  });
+
 export type RestaurantType = z.infer<typeof restaurantSchema>;
