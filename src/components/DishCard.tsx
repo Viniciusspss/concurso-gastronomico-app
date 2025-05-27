2a98c217c03fde99d6a90e95edd0dbff9e3b309b
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-26 22:32:18 -0300

    feat: mostrando imagem do prato atraves da rota de imagem

--- src/components/DishCard.tsx
+++ src/components/DishCard.tsx
@@ -1,5 +1,4 @@
 import { getAllDishesResponse } from "@/types/dishes";
-import pratoImage from "@/assets/pratoImage.jpg";
 import { StarIcon } from "lucide-react";
 
 type DishCardProps = {
@@ -7,14 +6,17 @@ type DishCardProps = {
 };
 
 export function DishCard({ dish }: DishCardProps) {
+
+
   return (
     <div className="w-auto bg-[#2C2C2C] hover:cursor-pointer">
       <div className="h-30 w-full overflow-hidden">
         <img
-          src={pratoImage}
+          src={`http://localhost:8080/api/uploads/${dish.image_url}`}
           alt="Foto do prato"
           className="w-full object-cover"
         />
+
       </div>
       <div className="flex flex-col items-start gap-2 px-3 py-3">
         <h1 className="font-bold text-amber-500">{dish.name}</h1>
