0ecb7795f62195b47044435492a645860f6674de
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-27 22:03:47 -0300

    refactor: altera layout do AuthLayout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom";
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logoWhite.png";
+import bgImage from "@/assets/backgroundImage.png"
 
 export function AuthLayout() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-[#F2F2F2]">
-      <div className="w-full absolute h-25 bg-[#D9CBA3] top-0 flex items-center gap-4" >
-        <img src={logo} alt="Logo" className="ml-8" />
-        <h1 className="font-bold text-[#A38C6D] text-xl">CONCURSO GASTRONÔMICO</h1>
+    <div className="flex relative h-screen w-full items-center  ">
+      <img src={bgImage} alt="Imagem de fundo" className="absolute w-full h-full z-[-1]" />
+      <div className="z-10 w-3/5 gap-10 ml-20">
+        <img src={logo} alt="Logo" />
+        <div className="flex flex-col">
+          <span className="text-4xl text-[var(--color-background)] font-bold">CONCURSO</span>
+          <span className="text-4xl text-[var(--color-primary)] font-bold">GASTRONÔMICO</span>
+        </div>
       </div>
-      <div className="w-[500px] px-4">
+      <div className="flex h-full w-2/5 ">
         <Outlet />
       </div>
     </div>
