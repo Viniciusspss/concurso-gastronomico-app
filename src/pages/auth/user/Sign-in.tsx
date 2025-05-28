99a1e53e70ca663d3d0d0a00ddbab15e2af90ba8
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-27 22:05:10 -0300

    style: altera estilo da tela de login de usuário

--- src/pages/auth/user/Sign-in.tsx
+++ src/pages/auth/user/Sign-in.tsx
@@ -42,44 +42,46 @@ export function SignIn() {
   }
 
   return (
-    <div>
-      <Button variant="beige" className="absolute top-8 right-8" asChild>
-        <Link to="/">Escolher outro tipo de login</Link>
-      </Button>
-      <DefaultForm className="bg-white p-15 rounded-2xl" onSubmit={handleSubmit(onSubmit)}>
-        <h1 className="flex w-full justify-center mb-10 font-bold text-xl">LOGIN</h1>
+    <div className="bg-[var(--color-background)] w-full justify-center items-center flex flex-col">
+      <div className="flex flex-col">
+        <h1 className="flex w-full justify-center font-bold text-xl text-[var(--text-primary)]">ENTRAR</h1>
+        <h2 className="flex w-full justify-center text-sm text-[var(--color-primary)]">Preencha as informações para prosseguir</h2>
+      </div>
+      <DefaultForm className=" p-15 rounded-2xl justify-center items-center" onSubmit={handleSubmit(onSubmit)}>
+
         <div className="flex flex-col gap-3">
-          <div className="flex gap-2">
-            <Label htmlFor="email">Email:</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="Digite seu email"
-              className="bg-[#ddd] "
-              {...register("email", { required: "Email é obrigatório" })}
-            />
+          <div className="flex">
+            <Label htmlFor="email" className="flex flex-col items-start text-[var(--text-muted)]">Email
+              <Input
+                id="email"
+                type="email"
+                className="bg-[var(--color-background)] border-[var(--text-muted)] w-80"
+                {...register("email", { required: "Email é obrigatório" })}
+              />
+            </Label>
           </div>
-          <div className="flex gap-1">
-            <Label htmlFor="password">Senha:</Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="Digite sua senha"
-              className="bg-[#ddd] "
-              {...register("password", { required: "Senha é obrigatório" })}
-            />
+          <div className="flex">
+            <Label htmlFor="password" className="flex flex-col items-start w-80 text-[var(--text-muted)]">Senha
+              <Input
+                id="password"
+                type="password"
+                className="bg-[var(--color-background)] border-[var(--text-muted)]"
+                {...register("password", { required: "Senha é obrigatório" })}
+              />
+            </Label>
           </div>
-          <p className="text-xs">
-            Ainda não tenho uma conta.{" "}
-            <Link to="/SignUp/User" className="font-bold text-red-500">
-              Criar conta
-            </Link>
-          </p>
         </div>
-        <Button type="submit" className="rounded-xl" variant="beige">
+        <Button type="submit" variant="default" className="w-80">
           Conecte-se
         </Button>
+        <p className="text-xs">
+          Ainda não tenho uma conta.{" "}
+          <Link to="/SignUp/User" className="font-bold text-red-500">
+            Criar conta
+          </Link>
+        </p>
       </DefaultForm>
     </div>
+
   );
 }
