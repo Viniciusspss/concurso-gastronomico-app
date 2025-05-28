7c68fcb97863e2ec7c89a160e5ae4d0636d9a3ae
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-27 21:16:41 -0300

    style: estiliza pagina de inicial de selecionar forma de login

--- src/pages/auth/SelectProfile.tsx
+++ src/pages/auth/SelectProfile.tsx
@@ -1,19 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import bgImage from "@/assets/backgroundImage.png"
+import logoImage from "@/assets/logoYellow.png"
+import logo from "../../assets/logo.png"
 
 export function SelectProfile() {
   return (
-    <div className="flex w-full flex-col items-center justify-center">
-      <h1 className="mb-4 text-2xl font-medium">
-        Escolha como deseja realizar login
-      </h1>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Button className="w-60 shadow-lg" variant="beige" asChild>
-          <Link to="/SignIn/Restaurant">Restaurante</Link>
-        </Button>
-        <Button className="w-60 shadow-lg" variant="beige" asChild>
-          <Link to="/SignIn/User">Cliente</Link>
-        </Button>
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-[var(--color-background)]">
+      <div className="w-full h-25  flex items-center gap-4 bg-[var(--color-background)]" >
+        <img src={logo} alt="Logo" className="ml-8" />
+        <h1 className="font-bold text-[#A38C6D] text-xl"><span className="text-[var(--text-primary)]">CONCURSO</span> <span className="text-[var(--color-primary)]">GASTRONÔMICO</span></h1>
+      </div>
+      <div className="flex relative z-1 w-full h-full flex-col items-center justify-center gap-4">
+        <img src={bgImage} alt="Imagem de fundo da tela inicial" className="absolute w-full h-full z-[-1]" />
+        <img src={logoImage} alt="Logo central" className="" />
+        <h1 className="mb-4 text-2xl font-medium">
+          <span className="font-bold text-[var(--color-background)] text-4xl">CONCURSO</span><span className="font-bold text-[var(--color-primary)] text-4xl"> GASTRONÔMICO</span>
+        </h1>
+        <div className="flex  items-center justify-center gap-4">
+          <Button size="lg" variant="default" asChild>
+            <Link to="/SignIn/Restaurant">Entrar como cliente</Link>
+          </Button>
+          <Button size="lg" variant="muted" asChild>
+            <Link to="/SignIn/User">Entrar como restaurante</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
