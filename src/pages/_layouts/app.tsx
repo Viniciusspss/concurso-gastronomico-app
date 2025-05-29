97452c035d28c3df872e225280540fef1e5d3e55
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-28 21:27:29 -0300

    style: altera cor de fundo do AppLayout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <div className="flex h-screen w-full flex-col items-center bg-[#1E1E1E]">
-      <div className="flex w-[80%] flex-col items-center justify-center px-4 py-8">
+    <div className="flex h-screen w-full flex-col items-center bg-[var(--color-background)]">
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
