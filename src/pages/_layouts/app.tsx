31fff85474bf4ff476dbdfc8ca22e1fac03ae09f
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-05-30 18:38:52 -0300

    style: ajusta style do appLayout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -2,10 +2,9 @@ import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <div className="flex h-screen w-full flex-col items-center ">
-      <div className="flex flex-col items-center justify-center">
-        <Outlet />
-      </div>
+    <div className="flex h-screen  w-full flex-col items-center ">
+      <Outlet />
+
     </div>
   );
 }
