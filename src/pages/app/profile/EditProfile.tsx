f22d0a33044a0c20050c489160d191fc8fa732ce
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-06-01 19:43:38 -0300

    feat: cria e estiliza formulario de editar perfil do cliente

--- src/pages/app/profile/EditProfile.tsx
+++ src/pages/app/profile/EditProfile.tsx
@@ -1,4 +1,4 @@
-import { EditProfileForm } from "@/components/EditProfileForm";
+import { EditProfileForm } from "@/components/EditClientForm";
 
 export function EditProfile() {
   return <EditProfileForm />;
