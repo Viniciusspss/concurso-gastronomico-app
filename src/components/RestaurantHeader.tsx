3f0c15cc56cc27e4ad71c371734e55dfe85d2992
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-06-01 19:45:13 -0300

    feat: cria componente RestaurantHeader

--- src/components/RestaurantHeader.tsx
+++ src/components/RestaurantHeader.tsx
@@ -1,24 +1,59 @@
-import { Link } from "react-router-dom";
-import { DefaultButton } from "./DefaultButton";
-import { DropDownMenu } from "./DropDownMenu";
+import { Link, useLocation } from "react-router-dom";
 import { useAppSelector } from "@/hooks/useAppSelector";
+import { Button } from "./ui/button";
+import { LogOutIcon, UserRound } from "lucide-react";
+import { logout } from "@/store/slices/authSlice/authSlice";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import logo from "@/assets/logo.png";
 
 export function RestaurantHeader() {
-  const { user } = useAppSelector(state => state.auth);
+  const { user } = useAppSelector((state) => state.auth);
+
+  const dispatch = useAppDispatch();
+  const location = useLocation();
   return (
-    <div className="flex w-full justify-between">
-      <Link to="/create-dish">
-        <DefaultButton >CADASTRAR PRATO</DefaultButton>
-      </Link>
-      <div className="flex gap-4">
-        <Link to={`/restaurant-dishes/${user?.id}`}>
-          <DefaultButton className="rounded-2xl text-xs">PRATOS</DefaultButton>
+    <div className="flex w-full items-center justify-between bg-[var(--color-background)]">
+      <div className="flex h-25 w-full items-center gap-4 bg-[var(--color-background)]">
+        <img src={logo} alt="Logo" className="ml-8" />
+        <h1 className="text-xl font-bold text-[#A38C6D] hover:cursor-default">
+          <span className="text-[var(--text-primary)]">CONCURSO</span>{" "}
+          <span className="text-[var(--color-primary)]">GASTRONÔMICO</span>
+        </h1>
+      </div>
+      <div className="mr-15 flex w-full items-center justify-end gap-7">
+        <Link
+          to={`/restaurant-dishes/${user?.id}`}
+          className={`transition hover:cursor-pointer hover:text-[var(--color-primary)] ${location.pathname === `/restaurant-dishes/${user?.id}` ? "border-b-4 border-[var(--color-primary)] text-[var(--color-primary)]" : ""}`}
+        >
+          PRATOS
         </Link>
-        <Link to="/about">
-          <DefaultButton className="rounded-2xl text-xs">SOBRE</DefaultButton>
+        <Link
+          className={`transition hover:cursor-pointer hover:text-[var(--color-primary)] ${location.pathname === "/restaurants" ? "border-b-4 border-[var(--color-primary)] text-[var(--color-primary)]" : ""}`}
+          to="/create-dish"
+        >
+          CADASTRAR PRATO
+        </Link>
+        <Link
+          className={`transition hover:cursor-pointer hover:text-[var(--color-primary)] ${location.pathname === "/about" ? "border-b-4 border-[var(--color-primary)] text-[var(--color-primary)]" : ""}`}
+          to="/about"
+        >
+          SOBRE
         </Link>
       </div>
-      <DropDownMenu />
+      <div className="flex items-center justify-center gap-2">
+        <Link to="/edit-restaurant-profile">
+          <UserRound className="h-8 w-8 rounded-full bg-[var(--color-primary)] p-1 text-[var(--text-primary)] transition hover:cursor-pointer hover:text-[var(--color-background)]" />
+        </Link>
+        <Button
+          className="mr-8 hover:cursor-pointer"
+          size="sm"
+          variant="warn"
+          onClick={() => dispatch(logout())}
+        >
+          <LogOutIcon />
+          Sair
+        </Button>
+      </div>
     </div>
   );
 }
