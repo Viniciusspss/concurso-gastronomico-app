3d8b6a9f9419c8773fafa08434473013194fd3de
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-06-04 10:14:46 -0300

    style: estiliza página about

--- src/pages/app/About.tsx
+++ src/pages/app/About.tsx
@@ -3,13 +3,15 @@ import { RestaurantHeader } from "@/components/RestaurantHeader";
 import { useAppSelector } from "@/hooks/useAppSelector";
 
 export function About() {
-  const { user } = useAppSelector(state => state.auth)
+  const { user } = useAppSelector((state) => state.auth);
   return (
-    <div className="flex items-center flex-col gap-15 bg-gradient-to-b from-[var(--color-background)] to-[var(--text-foreground)] h-screen">
+    <div className="flex h-screen flex-col items-center gap-15 bg-[var(--color-background)]">
       {user && "email" in user ? <DefaultHeader /> : <RestaurantHeader />}
-      <div className="flex w-[50%] flex-col gap-7 items-center">
-        <h1 className="text-3xl text-[var(--text-primary)]">SOBRE NÓS</h1>
-        <p className="text-[var(--text-foreground)]">
+      <h1 className="text-3xl font-bold text-[var(--color-primary)]">
+        SOBRE NÓS
+      </h1>
+      <div className="flex w-[50%] flex-col items-center gap-7 rounded-4xl bg-[var(--color-background)] p-8 text-[var(--text-primary)] shadow-2xl">
+        <p>
           O Cozinha Arretada é um projeto acadêmico desenvolvido com o objetivo
           de valorizar a gastronomia regional e dar visibilidade aos
           restaurantes locais, especialmente em cidades pequenas. Através desta
@@ -18,7 +20,7 @@ export function About() {
           alimentar da região e fortalecendo a conexão entre quem cozinha e quem
           aprecia uma boa comida.
         </p>
-        <p className="text-[var(--text-foreground)]">
+        <p>
           Mais do que uma simples vitrine, o site funciona como um espaço de
           descoberta e interação: usuários podem se cadastrar, avaliar pratos,
           conhecer novos sabores e acompanhar um ranking dos restaurantes mais
@@ -26,7 +28,7 @@ export function About() {
           recebem feedbacks valiosos e têm a chance de competir em um concurso
           gastronômico que celebra o que há de melhor na culinária local.
         </p>
-        <p className="text-[var(--text-foreground)]">
+        <p>
           Este projeto nasceu dentro de um contexto acadêmico, como parte de uma
           proposta universitária voltada ao desenvolvimento de soluções digitais
           com impacto social e cultural. Por isso, o Cozinha Arretada carrega
