b24b89e1ece13e97757c537e0b20f9e3a806e41c
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-06-04 09:58:46 -0300

    style: ajusta estilo do card de restaurante

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -2,6 +2,9 @@ import { useAppSelector } from "@/hooks/useAppSelector";
 import { Button } from "./ui/button";
 import { getAllRestaurantsType } from "@/types/user/restaurant";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { loadAllDishes } from "@/store/slices/dishSlice/dishThunks";
 
 type RestaurantCardProps = {
   restaurant: getAllRestaurantsType;
@@ -12,8 +15,13 @@ export function RestaurantCard({ restaurant }: RestaurantCardProps) {
   const restaurantDishes = dishes.filter((dish) => {
     return dish.restaurant.id === restaurant.id;
   }).length;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadAllDishes());
+  }, [dispatch]);
   return (
-    <div className="flex h-115 flex-col rounded-2xl border-2 border-[var(--color-primary)] bg-[var(--color-background)] hover:cursor-pointer">
+    <div className="mb-8 flex h-90 w-80 flex-col rounded-2xl border-2 border-[var(--color-primary)] bg-[var(--color-background)] hover:cursor-pointer">
       <div className="h-55 w-full">
         <img
           src={`http://localhost:8080/api${restaurant.image_url}`}
@@ -21,21 +29,27 @@ export function RestaurantCard({ restaurant }: RestaurantCardProps) {
           className="h-full w-full rounded-t-2xl object-cover"
         />
       </div>
-      <div className="flex flex-1 flex-col items-start justify-between gap-2 px-3 py-3">
+      <div className="flex w-full flex-1 flex-col items-start justify-between gap-2 px-3 py-3">
         <div className="mt-5 flex w-full">
           <div className="flex w-full flex-col items-center justify-center">
             <h1 className="text-xl font-bold text-[var(--text-primary)]">
               {restaurant.name}
             </h1>
-            <p className="text-sm text-[var(--text-muted)]">
-              {restaurantDishes} pratos cadastrados
-            </p>
+            {restaurantDishes === 1 ? (
+              <p className="text-sm text-[var(--text-muted)]">
+                {restaurantDishes} prato cadastrado
+              </p>
+            ) : (
+              <p className="text-sm text-[var(--text-muted)]">
+                {restaurantDishes} pratos cadastrados
+              </p>
+            )}
           </div>
         </div>
 
         <div className="flex w-full items-center justify-center">
           <Link to={`/view-restaurant-dishes/${restaurant.id}`}>
-            <Button variant="default" className="w-[97%] hover:cursor-pointer">
+            <Button variant="default" className="w-70 hover:cursor-pointer">
               Ver pratos
             </Button>
           </Link>
