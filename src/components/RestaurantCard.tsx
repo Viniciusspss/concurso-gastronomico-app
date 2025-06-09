9aca8115efba9d5e8458d688b73413b0bb61f748
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-06-09 00:34:40 -0300

    refactor: edita estilo e exclui botão do card de restaurante

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,10 +1,10 @@
 import { useAppSelector } from "@/hooks/useAppSelector";
-import { Button } from "./ui/button";
 import { getAllRestaurantsType } from "@/types/user/restaurant";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useAppDispatch } from "@/hooks/useAppDispatch";
 import { loadAllDishes } from "@/store/slices/dishSlice/dishThunks";
+
 
 type RestaurantCardProps = {
   restaurant: getAllRestaurantsType;
@@ -21,40 +21,40 @@ export function RestaurantCard({ restaurant }: RestaurantCardProps) {
     dispatch(loadAllDishes());
   }, [dispatch]);
   return (
-    <div className="mb-8 flex h-90 w-80 flex-col rounded-2xl border-2 border-[var(--color-primary)] bg-[var(--color-background)] hover:cursor-pointer">
-      <div className="h-55 w-full">
-        <img
-          src={`http://localhost:8080/api${restaurant.image_url}`}
-          alt="Foto do prato"
-          className="h-full w-full rounded-t-2xl object-cover"
-        />
-      </div>
-      <div className="flex w-full flex-1 flex-col items-start justify-between gap-2 px-3 py-3">
-        <div className="mt-5 flex w-full">
-          <div className="flex w-full flex-col items-center justify-center">
-            <h1 className="text-xl font-bold text-[var(--text-primary)]">
-              {restaurant.name}
-            </h1>
-            {restaurantDishes === 1 ? (
-              <p className="text-sm text-[var(--text-muted)]">
-                {restaurantDishes} prato cadastrado
-              </p>
-            ) : (
-              <p className="text-sm text-[var(--text-muted)]">
-                {restaurantDishes} pratos cadastrados
-              </p>
-            )}
+    <Link to={`/view-restaurant-dishes/${restaurant.id}`}>
+
+      <div className="mb-8 flex h-80 w-80 flex-col  shadow-2xl hover:cursor-pointer ">
+
+        <div className="h-55 w-full">
+          <img
+            src={`http://localhost:8080/api${restaurant.image_url}`}
+            alt="Foto do prato"
+            className="h-full w-full  object-cover"
+          />
+        </div>
+        <div className="flex w-full flex-1 flex-col items-start justify-between gap-2 px-3 py-3">
+          <div className=" flex w-full">
+            <div className="flex w-full flex-col  justify-center">
+              <h1 className="text-xl font-bold text-[var(--text-primary)]">
+                {restaurant.name}
+              </h1>
+              {restaurantDishes === 1 ? (
+                <p className="text-sm text-[var(--text-muted)]">
+                  {restaurantDishes} prato cadastrado
+                </p>
+              ) : (
+                <p className="text-sm text-[var(--text-muted)]">
+                  {restaurantDishes} pratos cadastrados
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className="flex w-full items-center justify-center">
+
           </div>
         </div>
-
-        <div className="flex w-full items-center justify-center">
-          <Link to={`/view-restaurant-dishes/${restaurant.id}`}>
-            <Button variant="default" className="w-70 hover:cursor-pointer">
-              Ver pratos
-            </Button>
-          </Link>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 }
